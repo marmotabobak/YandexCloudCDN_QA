182d74962726f23f1b627fec5895ba8111068d23
Igor Urvantsev <iurvantsev@inno.tech>
2024-12-20 13:42:39 +0300

    refactor and randomly_curl_resources fix

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -22,6 +22,8 @@ from app.model import EntityName, APIEntity, EdgeCacheSettings, QueryParamsOptio
 logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 logger = logging.getLogger(__name__)
 
+# TODO: implement negative tests
+
 class RevalidatedTooEarly(Exception):
     # def __init__(self, message: str):
     #     super().__init__(message)
@@ -86,7 +88,7 @@ EDGE_CACHE_HOSTS = {
     'mar-srv05.yccdn.cloud.yandex.net': '188.72.105.6',
 }
 
-def repeat_several_times_with_pause_until_success_ot_timeout(attempts: int = 20, attempt_delay: int = 15):
+def repeat_until_success_or_timeout(attempts: int = 20, attempt_delay: int = 15):
     def decorator(func: Callable):
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
@@ -95,14 +97,14 @@ def repeat_several_times_with_pause_until_success_ot_timeout(attempts: int = 20,
                 try:
                     res = func(*args, **kwargs)
                     return res
-                except AssertionError:
+                except (AssertionError, RevalidatedTooEarly):
                     logger.debug(f'...failed. Sleeping for {attempt_delay} seconds...')
                     time.sleep(attempt_delay)
             pytest.fail('All attempts failed.')
         return wrapper
     return decorator
 
-@repeat_several_times_with_pause_until_success_ot_timeout()
+@repeat_until_success_or_timeout()
 def resources_are_not_alive_for_period_of_time(
         cnames: List[str],
         duration_to_success: int = API_DELAY,
@@ -145,7 +147,7 @@ def resources_are_not_alive_for_period_of_time(
     return res
 
 
-@repeat_several_times_with_pause_until_success_ot_timeout()
+@repeat_until_success_or_timeout()
 def resources_are_equal_for_period_of_time(
         resources_proc: ResourcesAPIProcessor,
         resources: List[CDNResource],
@@ -190,6 +192,8 @@ class TestCDN:
         cls.resources = []
         cls.cdn_cnames = []  # for making new resources only
 
+        cls.method_to_curl_resources = cls.randomly_curl_resources
+
         cls.resources_proc = ResourcesAPIProcessor(
             entity_name=EntityName.CDN_RESOURCE,
             api_url=API_URL,
@@ -203,7 +207,7 @@ class TestCDN:
         if not cls.origin_is_available_with_200():
             pytest.fail('Origin is not available')
 
-        if edges := cls.get_edge_caches_that_are_not_available_by_ping():
+        if edges := cls.get_not_pinged_edges():
             logging.warning(f'Edges are not pinged successfully: {edges}')
 
         cls.initialize_resources()
@@ -323,24 +327,29 @@ class TestCDN:
         assert True
 
     # TODO: make random check but with return once success
-    @classmethod
     # TODO: make async otherwise too few requests and needs too many periods_count (such as 5) to assert successfully
     # NB! better use targeted requests to specific cache hosts
-    def randomly_http_get_request_resources_within_period_of_time(
+    @classmethod
+    def randomly_curl_resources(
             cls,
             resources: List[CDNResource],
+            protocol: str = 'http',
             period_of_time: int = SHORT_TTL,
-            periods_count: int = EDGE_CACHE_PERIODS_TO_TEST
+            periods_count: int = EDGE_CACHE_PERIODS_TO_TEST,
+            add_query_arg: bool = False
     ) -> bool:
         start_time = time.time()
         resources_statuses = {}
+        query_generator = increment()
 
         time_to_test = periods_count * period_of_time
         logger.info(f'GET resources [{[r.cname for r in resources]}] for {time_to_test} seconds...')
 
         while time.time() < start_time + time_to_test:
             for resource in resources:
-                url = f'http://{resource.cname}'
+                url = f'{protocol}://{resource.cname}'
+                if add_query_arg:
+                    url += '?foo=' + str(next(query_generator))
                 response = requests.get(url)
                 response_headers = EdgeResponseHeaders(**response.headers)
                 if not response_headers.cache_status:
@@ -359,7 +368,7 @@ class TestCDN:
 
         for resource_id, resource_statuses in resources_statuses.items():
             for host_name, host_responses in resource_statuses.items():
-                if cls.resource_is_correctly_processed_by_edge_cache(host_responses):
+                if cls.resource_is_correctly_processed_by_edge(statuses=host_responses, period_of_time=period_of_time):
                     resources_statuses_copy[resource_id].pop(host_name)
             if not resources_statuses_copy[resource_id]:
                 resources_statuses_copy.pop(resource_id)
@@ -369,9 +378,10 @@ class TestCDN:
         return resources_statuses_copy == {}
 
     @classmethod
+    # TODO: refactor - too long method
     # TODO: make async otherwise too few requests and needs too many periods_count (such as 5) to assert successfully
     # NB! better use targeted requests to specific cache hosts
-    def targeted_http_get_request_resources_within_period_of_time(
+    def targeted_http_curl_resources(
             cls,
             resources: List[CDNResource],
             period_of_time: int = SHORT_TTL,
@@ -424,7 +434,7 @@ class TestCDN:
                             host_response = HostResponse(time=time.time(), status=response_headers.cache_status)
                             resources_statuses[resource.id][response_headers.cache_host].append(host_response)
 
-                            if cls.resource_is_correctly_processed_by_edge_cache(
+                            if cls.resource_is_correctly_processed_by_edge(
                                 resources_statuses[resource.id][response_headers.cache_host],
                                 period_of_time=period_of_time
                             ):
@@ -439,7 +449,7 @@ class TestCDN:
         return False
 
     @staticmethod
-    def resource_is_correctly_processed_by_edge_cache(
+    def resource_is_correctly_processed_by_edge(
             statuses: List[HostResponse],
             period_of_time: int = SHORT_TTL,
             error_rate: float = 0.9
@@ -461,7 +471,7 @@ class TestCDN:
         return False
 
     @staticmethod
-    def get_edge_caches_that_are_not_available_by_ping() -> List[str]:
+    def get_not_pinged_edges() -> List[str]:
         res = []
         for edge_host in EDGE_CACHE_HOSTS:
             if not ping(edge_host, attempts=2):
@@ -476,7 +486,7 @@ class TestCDN:
         assert self.resources and all(r.id for r in self.resources), 'CDN resources not created'
 
     @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    @repeat_until_success_or_timeout()
     def test_active_and_not_active_resources(self):
         for resource in self.resources:
             url = f'http://{resource.cname}'
@@ -489,7 +499,7 @@ class TestCDN:
                 assert request_code == 404, f'CDN resource {request_code}, should be 404'
 
     @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    @repeat_until_success_or_timeout()
     def test_ip_address_acl(self):
         for resource in self.resources:
             url = f'http://{resource.cname}'
@@ -505,7 +515,7 @@ class TestCDN:
                 assert request_code != 403, f'CDN resource 403, should be not'
 
     @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    @repeat_until_success_or_timeout()
     def test_edge_cache_settings_enabled_revalidate_out_of_ttl(self):
         resources_to_test = []
         for resource in self.resources:  # selecting all resources with edge_cache_settings with value EDGE_CACHE_VALUE_TO_TEST
@@ -527,12 +537,12 @@ class TestCDN:
         if not resources_to_test:
             pytest.fail(f'No resources to test edge_cache_settings with value [{SHORT_TTL}] enabled')
 
-        assert self.targeted_http_get_request_resources_within_period_of_time(
+        assert self.method_to_curl_resources(
             resources=resources_to_test
         ), 'Not all statuses were processed correctly'
 
     # @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    @repeat_until_success_or_timeout()
     def test_edge_cache_settings_enabled_revalidate_too_early_error(self):
         resources_to_test = []
         for resource in self.resources:  # selecting all resources with edge_cache_settings with value EDGE_CACHE_VALUE_TO_TEST
@@ -555,12 +565,10 @@ class TestCDN:
             pytest.fail(f'No resources to test edge_cache_settings with value [{SHORT_TTL}] enabled')
 
         with pytest.raises(RevalidatedTooEarly):
-            self.targeted_http_get_request_resources_within_period_of_time(
-                resources=resources_to_test, period_of_time=SHORT_TTL * 2
-            )
+            self.method_to_curl_resources(resources=resources_to_test, period_of_time=2*SHORT_TTL)
 
-    @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    # @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
+    @repeat_until_success_or_timeout()
     def test_edge_cache_settings_enabled_do_not_revalidate_within_ttl(self):
         resources_to_test = []
         for resource in self.resources:  # selecting all resources with edge_cache_settings with value EDGE_CACHE_VALUE_TO_TEST
@@ -583,12 +591,12 @@ class TestCDN:
             pytest.fail(f'No resources to test edge_cache_settings '
                         f'with value [{LONG_TTL}] enabled')
 
-        assert not self.targeted_http_get_request_resources_within_period_of_time(
+        assert not self.method_to_curl_resources(
             resources=resources_to_test
         ), 'Not all statuses were processed correctly'
 
-    @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    # @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
+    @repeat_until_success_or_timeout()
     def test_edge_cache_settings_disabled(self):
         resources_to_test = []
         for resource in self.resources:  # selecting all resources with edge_cache_settings with value EDGE_CACHE_VALUE_TO_TEST
@@ -616,8 +624,8 @@ class TestCDN:
             request_headers = EdgeResponseHeaders(**request.headers)
             assert not request_headers.cache_status
 
-    @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    # @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
+    @repeat_until_success_or_timeout()
     def test_ignore_query_string(self):
         resources_to_test = []
         for resource in self.resources:  # selecting all resources with edge_cache_settings with value EDGE_CACHE_VALUE_TO_TEST
@@ -644,10 +652,10 @@ class TestCDN:
             pytest.fail(f'No resources found to test disabled edge_cache_settings')
 
         logger.info(f'GET resources [{[r.cname for r in resources_to_test]}]...')
-        assert self.targeted_http_get_request_resources_within_period_of_time(resources_to_test, add_query_arg=True)
+        assert self.method_to_curl_resources(resources_to_test, add_query_arg=True)
 
-    @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
-    @repeat_several_times_with_pause_until_success_ot_timeout()
+    # @pytest.mark.skip('FOR DEBUG ONLY - ACTIVATE FOR PRODUCTION USE')
+    @repeat_until_success_or_timeout()
     def test_do_not_ignore_query_string(self):
         resources_to_test = []
         for resource in self.resources:
@@ -672,5 +680,5 @@ class TestCDN:
             pytest.fail(f'No resources found to test disabled edge_cache_settings')
 
         logger.info(f'GET resources [{[r.cname for r in resources_to_test]}]...')
-        assert not self.targeted_http_get_request_resources_within_period_of_time(resources_to_test, add_query_arg=True)
-
+        with pytest.raises(RevalidatedTooEarly):
+            self.method_to_curl_resources(resources_to_test, add_query_arg=True)
